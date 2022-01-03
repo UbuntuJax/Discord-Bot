3b196a81e90a8891c892a2160ff578cf3ec8ce9d
UbuntuJax <jacksonbledsoe@gmail.com>
2022-01-03 12:03:59 +1000

    implemented zen.io 22.24
    Added requests library to python

--- main.py
+++ main.py
@@ -2,8 +2,16 @@
 #I want a bot which pulls random quotes from the quotes chat and sends them to whichever channel the user is in when someone types '!IShitPant'
 import discord
 import os
+import requests
+import json
 
 client = discord.Client()
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return(quote)
 
 @client.event
 async def on_ready():
@@ -14,8 +22,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
 
 
 client.run(os.getenv('TOKEN'))
