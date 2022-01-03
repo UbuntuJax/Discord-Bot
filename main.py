27de79e6b1a3a813cf4968c3280b016cd571d41f
UbuntuJax <jacksonbledsoe@gmail.com>
2022-01-03 11:56:04 +1000

    21.55 - Discord bot turns on

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-# Robot Password: OTI3MDk4NzgwNTIyNzkwOTc0.YdFSBQ.Hjft2JU_pFhyDmXKYGHaz9Ek8wk
+#Robot Password: OTI3MDk4NzgwNTIyNzkwOTc0.YdFSBQ.Hjft2JU_pFhyDmXKYGHaz9Ek8wk
+#I want a bot which pulls random quotes from the quotes chat and sends them to whichever channel the user is in when someone types '!IShitPant'
 import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event 
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(os.getenv('TOKEN'))
