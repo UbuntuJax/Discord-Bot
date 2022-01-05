b4dcf45cb5af2710a2aecfbda45e9a0adb799aa5
UbuntuJax <jacksonbledsoe@gmail.com>
2022-01-05 16:44:26 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import random
 
 PREFIX = ("$")
-bot = commands.Bot(command_prefix=PREFIX, description='Hi')
+bot = commands.Bot(command_prefix=PREFIX, description='Cock and Balls')
 
 
 @bot.event
@@ -18,5 +18,18 @@ async def on_ready():
     activity = discord.Game(name=bot_name, type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     print('We have logged in as {0.user}'.format(bot))
+
+@bot.event
+async def on_message(message):
+    msg = message.content
+    if message.author == bot.user:
+        return
+    
+    doxx_activation = ["dylan", "flyingluigis", "lewongles", "cunt"]
+    doxx_messages = ["24 Horder", "Horder Avenue", "Dylan used to be one of the best OCE D.Vas. Point and laugh.", "https://www.google.com/maps/place/24+Horder+Ave,+Labrador+QLD+4215/@-27.9503754,153.3973667,3a,75y,174.26h,90t/data=!3m7!1e1!3m5!1sMHatMFIqA2iLPL2hsZnadg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DMHatMFIqA2iLPL2hsZnadg%26cb_client%3Dsearch.gws-prod.gps%26w%3D86%26h%3D86%26yaw%3D174.25719%26pitch%3D0%26thumbfov%3D100!7i13312!8i6656!4m5!3m4!1s0x6b910fde80b478f3:0x5d728e1be878fe4b!8m2!3d-27.9505861!4d153.3973861", \
+        "GET ON POINT!!!", "FUCKING SHOOT HIM!!!"]
+    if any(word in msg for word in doxx_activation):
+        await message.channel.send(random.choice(doxx_messages))
+    
 
 bot.run(os.getenv('TOKEN'))
