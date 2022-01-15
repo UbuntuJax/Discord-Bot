db2c0ba2f77d3723bb51a0c37cc6bc32b2cf18f2
UbuntuJax <jacksonbledsoe@gmail.com>
2022-01-15 17:41:22 +1000

    bot status updated

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix=PREFIX, description='Cock and Balls')
 doxx_word_blacklist = []
 bot_name_blacklist = []
 bot_name_list = ["Shitting Brock's Pants", "Googling Funny Monkee", "Cock and Balls", "Doxxing Dylan", "https://www.youtube.com/user/JordanPetersonVideos", \
-        "https://www.youtube.com/watch?v=1XNoiP_nZgs"]
+        "https://www.youtube.com/watch?v=1XNoiP_nZgs", "please someone play overwatch just once bro"]
 doxx_activation = ["dylan", "flyingluigis", "lewongles", "cunt"]
 doxx_messages = ["24 Horder", "Horder Avenue", "Dylan used to be one of the best OCE D.Vas. Point and laugh.", "https://www.google.com/maps/place/24+Horder+Ave,+Labrador+QLD+4215/@-27.9503754,153.3973667,3a,75y,174.26h,90t/data=!3m7!1e1!3m5!1sMHatMFIqA2iLPL2hsZnadg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DMHatMFIqA2iLPL2hsZnadg%26cb_client%3Dsearch.gws-prod.gps%26w%3D86%26h%3D86%26yaw%3D174.25719%26pitch%3D0%26thumbfov%3D100!7i13312!8i6656!4m5!3m4!1s0x6b910fde80b478f3:0x5d728e1be878fe4b!8m2!3d-27.9505861!4d153.3973861", \
     "GET ON POINT!!!", "FUCKING SHOOT HIM!!!"]
